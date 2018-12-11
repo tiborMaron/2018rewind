98677705939eb96800efb3ee4661cbe79d2ca797
danielszakacs <daniel.sokach@gmail.com>
2018-12-11 11:33:04 +0100

    SpringBootApplication main method created

--- google-service/src/main/java/com/codecool/google_service/GoogleServiceApp.java
+++ google-service/src/main/java/com/codecool/google_service/GoogleServiceApp.java
@@ -1,4 +1,12 @@
 package com.codecool.google_service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GoogleServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleServiceApp.class, args);
+    }
 }
