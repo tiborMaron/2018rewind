0bd7904ba3eedd35e6c60fbf8b35be01e2ac83d3
“elmerzsolt” <drogentm@gmail.com>
2018-12-11 11:51:46 +0100

    Social media components created.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,12 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
+import { YoutubeComponent } from './youtube/youtube.component';
+import { SpotifyComponent } from './spotify/spotify.component';
+import { GoogleComponent } from './google/google.component';
+import { NetflixComponent } from './netflix/netflix.component';
+import { YoutubeResultsComponent } from './youtube/youtube-results/youtube-results.component';
+import { SpotifyResultsComponent } from './spotify/spotify-results/spotify-results.component';
+import { GoogleResultsComponent } from './google/google-results/google-results.component';
+import { NetflixResultsComponent } from './netflix/netflix-results/netflix-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    YoutubeComponent,
+    SpotifyComponent,
+    GoogleComponent,
+    NetflixComponent,
+    YoutubeResultsComponent,
+    SpotifyResultsComponent,
+    GoogleResultsComponent,
+    NetflixResultsComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/src/app/google/google-results/google-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-results',
+  templateUrl: './google-results.component.html',
+  styleUrls: ['./google-results.component.css']
+})
+export class GoogleResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
