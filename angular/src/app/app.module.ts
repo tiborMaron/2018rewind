a2a8f514d1f809a45dc40915875d383f618cafe3
Milan Kiszely <milankiszely@gmail.com>
2018-12-11 12:10:39 +0100

    Added slider

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
+import { SliderComponent } from './header/navbar/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
