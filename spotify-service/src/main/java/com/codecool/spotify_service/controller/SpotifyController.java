81e388bb7de5193bce633562fbe266f117a10887
davidlenner <david910128@gmail.com>
2018-12-13 17:08:51 +0100

    refactored. when endpoint called it was slow cause had to read all the files always.

--- spotify-service/src/main/java/com/codecool/spotify_service/controller/SpotifyController.java
+++ spotify-service/src/main/java/com/codecool/spotify_service/controller/SpotifyController.java
@@ -1,6 +1,6 @@
 package com.codecool.spotify_service.controller;
 
-import com.codecool.spotify_service.init.InitFiles;
+import com.codecool.spotify_service.init.InitDB;
 import com.codecool.spotify_service.service.SpotifyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,19 +15,15 @@ public class SpotifyController {
     @Autowired
     SpotifyService spotifyService;
 
-    InitFiles initFiles = new InitFiles();
+    InitDB initFiles = new InitDB();
 
     @GetMapping("/get-toplist")
     public String getTopList() {
-        spotifyService.selectFiles(initFiles.getFilenames());
-        spotifyService.getTopHundred();
         return spotifyService.getFirstTen();
     }
 
     @GetMapping("/get-toplist/{page}")
     public String getTracksByPage(@PathVariable String page){
-        spotifyService.selectFiles(initFiles.getFilenames());
-        spotifyService.getTopHundred();
         return spotifyService.getPage(page);
     }
 }
--- spotify-service/src/main/java/com/codecool/spotify_service/service/SpotifyService.java
+++ spotify-service/src/main/java/com/codecool/spotify_service/service/SpotifyService.java
@@ -1,111 +1,23 @@
 package com.codecool.spotify_service.service;
 
+import com.codecool.spotify_service.init.InitDB;
 import com.codecool.spotify_service.model.Track;
 import com.google.gson.Gson;
-import com.opencsv.CSVReader;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class SpotifyService {
 
-    private HashMap<String, Track> tracks = new HashMap<>();
-    private List<Track> toptracks = new ArrayList<>();
+    private InitDB initDB;
 
-    public void selectFiles(List<String> filenames){
-        tracks.clear();
-        toptracks.clear();
-        for (String name: filenames){
-            this.readFile(name);
-        }
-    }
-
-    public void readFile(String filename){
-
-        try {
-            File file = new File("/home/david/codecool/java-ee/2018rewind/spotify-service/src/main/resources/csv/" + filename + ".csv");
-
-            FileReader filereader = new FileReader(file);
-            CSVReader csvReader = new CSVReader(filereader);
-            String[] nextRecord;
-            List<String> tracksdata = new ArrayList<>();
-
-            while ((nextRecord = csvReader.readNext()) != null) {
-                for (String cell : nextRecord) {
-                    tracksdata.add(0,cell);
-                }
-            }
-            createTracks(tracksdata);
-            tracksdata.clear();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void createTracks(List<String> tracks){
-
-        int counter = 5;
-        for (int i = 0; i < tracks.size()-5; i++) {
-            if (counter == 5){
-                if (this.tracks.containsKey(tracks.get(i+3))){
-                    this.tracks.get(tracks.get(i+3)).setStream(this.tracks.get(tracks.get(i+3)).getStream()+ Long.parseLong(tracks.get(i+1)));
-                }else{
-                    this.tracks.put(tracks.get(i+3),new Track(Integer.parseInt(tracks.get(i+4)),tracks.get(i+3),tracks.get(i+2),Integer.parseInt(tracks.get(i+1)),tracks.get(i)));
-                }
-            }else if (counter == 1){
-                counter = 5;
-                continue;
-            }
-            counter--;
-        }
-    }
-    public void getImageUrl(List<Track> tracks) throws IOException {
-
-        for (Track track: tracks) {
-            Document doc = Jsoup.connect(track.getUrl()).get();
-            int counter = 0;
-            for(Element meta : doc.select("meta")) {
-                if (counter == 9){
-                    track.setImage(meta.attr("content"));
-                    break;
-                }
-                counter++;
-            }
-        }
-    }
-
-    public void getTopHundred(){
-
-        List<Long> topnumber = new ArrayList<>();
-
-        for(Map.Entry<String, Track> entry : tracks.entrySet()) {
-            Track value = entry.getValue();
-            topnumber.add(value.getStream());
-        }
-        Collections.sort(topnumber);
-        Collections.reverse(topnumber);
-        for (int i = 0; i < 100; i++){
-            for(Map.Entry<String, Track> entry : tracks.entrySet()) {
-                Track value = entry.getValue();
-                if (value.getStream() == topnumber.get(i)){
-                    toptracks.add(0,value);
-                }
-            }
-        }
-        Collections.reverse(toptracks);
-        try {
-            getImageUrl(toptracks);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public SpotifyService(InitDB initDB) {
+        this.initDB = initDB;
+        initDB.selectFiles(initDB.getFilenames());
+        initDB.getTopHundred();
     }
 
     public String parseListToJson(List<Track> tracks) {
@@ -113,7 +25,7 @@ public class SpotifyService {
     }
 
     public String getFirstTen() {
-        List<Track> tracks = toptracks;
+        List<Track> tracks = initDB.getToptracks();
         List<Track> topten = new ArrayList<>();
         for (int i = 9; i > 0; i--) {
             topten.add(tracks.get(i));
@@ -123,7 +35,7 @@ public class SpotifyService {
     }
     
     public String getPage(String page){
-        List<Track> tracks = toptracks;
+        List<Track> tracks = initDB.getToptracks();
         List<Track> pageData = new ArrayList<>();
         if (page.equals("1")){
             return getFirstTen();
