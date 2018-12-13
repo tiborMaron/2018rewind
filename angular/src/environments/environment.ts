6dcb9bb966111f572df85b2eaaa3d97e5ee2b036
Milan Kiszely <milankiszely@gmail.com>
2018-12-13 11:03:30 +0100

    Updated ip's

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  googleURI: 'http://192.168.160.226:60011',
-  spotifyURI: 'http://192.168.160.89:60013'
+  googleURI: 'http://192.168.162.44:60011',
+  spotifyURI: 'http://192.168.162.45:60013',
+  youtubeURI: 'http://192.168.162.47:60014'
 };
 
 /*
