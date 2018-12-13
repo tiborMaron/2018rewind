1654d6516538fe1d17428ce9a9331d449814c42c
Milan Kiszely <milankiszely@gmail.com>
2018-12-13 11:03:05 +0100

    Added youtube data fetch from microservice

--- angular/src/app/youtube/youtube-result/youtube-result.component.ts
+++ angular/src/app/youtube/youtube-result/youtube-result.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {YoutubeResult} from "../../model/YoutubeResult";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-youtube-result',
@@ -9,10 +10,16 @@ import {YoutubeResult} from "../../model/YoutubeResult";
 export class YoutubeResultComponent implements OnInit {
 
   @Input() result: YoutubeResult;
+  @Input() rank: number;
+  videoURL: SafeResourceUrl;
 
-  constructor() { }
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
+    this.videoURL =
+      this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.result.id);
   }
 
 }
