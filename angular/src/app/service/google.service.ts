2fb1b887d39204d4e605269aa0c63296aa563069
Milan Kiszely <milankiszely@gmail.com>
2018-12-13 10:10:50 +0100

    Removed mock

--- angular/src/app/service/google.service.ts
+++ angular/src/app/service/google.service.ts
@@ -11,14 +11,12 @@ export class GoogleService {
 
   private serverUri = environment.googleURI;
 
-  private mock : GoogleResult[] = [{title: "title1", rank: 1}, {title: "title2", rank: 2}, {title: "title3", rank: 3}];
-
   getResults(): Observable<GoogleResult[]>{
     return this.http.get<GoogleResult[]>(this.serverUri + "/get-toplist");
   }
 
   getResultsByPage(page: number): Observable<GoogleResult[]> {
-    return of(this.mock);
+    return null;
   }
 
   constructor(
